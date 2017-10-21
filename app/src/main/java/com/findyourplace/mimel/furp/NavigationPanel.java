3485c17692142e42478e759f16ac46577475c6c5
Miller <millergarciaq@gmail.com>
2017-10-20 23:55:40 -0500

    put logout in navigation panel menu

--- app/src/main/java/com/findyourplace/mimel/furp/NavigationPanel.java
+++ app/src/main/java/com/findyourplace/mimel/furp/NavigationPanel.java
@@ -132,8 +132,11 @@ public class NavigationPanel extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+
         }
 
         return super.onOptionsItemSelected(item);
